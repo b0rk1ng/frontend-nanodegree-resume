e71607fd6bb11b61e40f1bcd4f0affc20182e786
Matthew Ashman <matthewiashman@gmail.com>
2022-07-03 01:14:47 +0100

    Add variables for name

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,5 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var firstName = "Matthew";
+var lastName = "Ashman";
+var fullName = firstName + " " + lastName;
 
-$("#header").append("<h1>Matthew Ashman</h1>");
+$("#header").append(`<h1>${fullName}</h1>`);
