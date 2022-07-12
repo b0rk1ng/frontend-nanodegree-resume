7312b64db2654abe2532867f2c54fda31c83d75d
Matthew Ashman <matthewiashman@gmail.com>
2022-07-13 00:16:56 +0100

    Add name using formattedName varible rather than direct in h1 tag

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,4 +4,6 @@ This is empty on purpose! Your code to build the resume will go here.
 
 let name = "Matthew Ashman";
 
-$("#header").append(`<h1>${name}</h1>`);
+let formattedName = HTMLheaderName.replace('%data%', name);
+
+$('#header').append(formattedName);
