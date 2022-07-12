b54c13336b209ccdeca891737b08c74327b2caee
Matthew Ashman <matthewiashman@gmail.com>
2022-07-13 00:20:28 +0100

    Add role to header using formattedRole variable

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,7 +3,10 @@ This is empty on purpose! Your code to build the resume will go here.
 */
 
 let name = "Matthew Ashman";
+let role = "Infrastructure Engineer";
 
 let formattedName = HTMLheaderName.replace('%data%', name);
+let formattedRole = HTMLheaderRole.replace('%data%', role);
 
 $('#header').append(formattedName);
+$('#header').append(formattedRole);
