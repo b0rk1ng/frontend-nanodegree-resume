85b8d5dc341eb9eff8688073da78cc91e15934ca
Matthew Ashman <matthewiashman@gmail.com>
2022-07-13 00:24:53 +0100

    Set name and role inside bio object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,11 +2,13 @@
 This is empty on purpose! Your code to build the resume will go here.
 */
 
-let name = "Matthew Ashman";
-let role = "Infrastructure Engineer";
+let bio = {
+    name : "Matthew Ashman",
+    role : "Infrastructure Engineer"
+}
 
-let formattedName = HTMLheaderName.replace('%data%', name);
-let formattedRole = HTMLheaderRole.replace('%data%', role);
+let formattedName = HTMLheaderName.replace('%data%', bio.name);
+let formattedRole = HTMLheaderRole.replace('%data%', bio.role);
 
 $('#header').append(formattedName);
 $('#header').append(formattedRole);
