ea0127b775308a68b38391efc9ad797131e4b2dd
Matthew Ashman <matthewiashman@gmail.com>
2023-05-10 17:25:19 +0100

    Change lets to consts and change RCDTS to HMRC

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,17 +2,17 @@
   This is empty on purpose! Your code to build the resume will go here.
 */
 
-let bio = {
+const bio = {
     name : "Matthew Ashman",
     role : "Infrastructure Engineer",
     mobile: "07403928289",
     email: "matthewiashman@gmail.com",
     pic: "images/moi.jfif",
     welcome: "A passionate, highly motivated and dedicated professional with strong technical, organisational and optimisation skills. Experienced with ServiceNow platform and with troubleshooting IT issues and managing a variety of data for a wide range of purposes.",
-    skills: ["ServiceNow", "Python", "Linux", "JavaScript", "HTML/CSS", "SQL", "AWS", "Git", "Ansible", "Terraform"]
+    skills: ["ServiceNow", "PowerShell", "Python", "Linux", "JavaScript", "HTML/CSS", "SQL", "AWS", "Git", "Ansible", "Terraform"]
 }
 
-let education = {
+const education = {
     'school': "University of Liverpool",
     'start': 2006,
     'degree': "Mathematical Physics",
@@ -21,25 +21,25 @@ let education = {
     'major': "String Theory"
 }
 
-let work = {
-    'employer': "RCDTS (for HMRC)",
+const work = {
+    'employer': "HMRC",
     'start': "January 2022",
     'title': bio.role,
     'location': "Telford",
     'description': "Implementing enterprise tooling solutons, such as Configuration of integrations between ServiceNow and other applications, for example Nexthink digital experience monitoring and set up of ServiceNow data ingestion via AWS S3. Supporting enterprise tooling solutions including out of hours support."
 }
 
-let formattedName = HTMLheaderName.replace('%data%', bio.name);
-let formattedRole = HTMLheaderRole.replace('%data%', bio.role);
-let formattedMobile = HTMLmobile.replace('%data%', bio.mobile);
-let formattedEmail = HTMLemail.replace('%data%', bio.email);
-let formattedBioPic = HTMLbioPic.replace('%data%', bio.pic);
-let formattedWelcome = HTMLwelcomeMsg.replace('%data%', bio.welcome);
+const formattedName = HTMLheaderName.replace('%data%', bio.name);
+const formattedRole = HTMLheaderRole.replace('%data%', bio.role);
+const formattedMobile = HTMLmobile.replace('%data%', bio.mobile);
+const formattedEmail = HTMLemail.replace('%data%', bio.email);
+const formattedBioPic = HTMLbioPic.replace('%data%', bio.pic);
+const formattedWelcome = HTMLwelcomeMsg.replace('%data%', bio.welcome);
 let formattedSkills = bio.skills.map((skill) => HTMLskills.replace('%data%', skill));
 formattedSkills = HTMLskillsStart.replace('%data%', formattedSkills);
 
-let formattedEducationSchool = HTMLschoolName.replace('%data%', education.school);
-let formattedWorkJob = HTMLworkTitle.replace('%data%', work['title']);
+const formattedEducationSchool = HTMLschoolName.replace('%data%', education.school);
+const formattedWorkJob = HTMLworkTitle.replace('%data%', work['title']);
 
 $('#header').append(formattedName);
 $('#header').append(formattedRole);
